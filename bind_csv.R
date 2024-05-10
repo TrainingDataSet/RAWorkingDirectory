48c436badc7aa3a363395653b0221db0fc16b991
NamHun_Kim <kimnh12200@gmail.com>
2024-05-10 05:15:24 +0000

    .

---
+++ bind_csv.R
@@ -0,0 +1,24 @@
+# [Last Modified: 2024-05-09]
+
+library(dplyr)
+library(readr)
+
+setwd("~/RAWorkingDirectory")
+
+bind_csv <- function(directory, file_name) {
+  
+  # Get a list of all CSV files in that directory
+  file_list <- list.files(directory, pattern = "\\.csv$", full.names = TRUE)
+  
+  # Read all CSV files and merge them into one data frame
+  combined_data <- lapply(file_list, read_csv) %>%
+    bind_rows()
+  
+  write_csv(combined_data, file_name)
+}
+
+# Run
+directory = "BT_pollutants"
+file_name = "BT_pollutants/BT_pollutants.csv"
+bind_csv(directory, file_name)
+
